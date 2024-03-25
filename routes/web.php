e27ee4fba872065d41ef75888ff8401bad17baee
Mohamed Sorour <mas.qa77@gmail.com>
2024-03-25 02:28:47 +0200

    - Login using mobile number

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/mobile-sign-in', function () {
+    return view('mobile-sign-in');
 });
